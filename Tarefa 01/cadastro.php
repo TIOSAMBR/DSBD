39de2aab2627d0724a085743f2542ebfa4524380
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2024-02-22 09:18:44 -0300

    Update cadastro.php

--- Tarefa 01/cadastro.php
+++ Tarefa 01/cadastro.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+$preco_step = 0.01;
+$preco_min = 0;
+$preco_max = 5000;
+$estoque_min = 0;
+$estoque_max = 10000;
+$required = "required"; 
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,10 +18,10 @@
 <body>
     <h2>Cadastro de Produto</h2>
     <form action="dados.php" method="post">
-        Nome do Produto: <input type="text" name="nome" required><br>
-        Descrição do Produto: <textarea name="descricao" required></textarea><br>
-        Preço do Produto: <input type="number" name="preco" step="0.01" min="0" max="5000" required><br>
-        Quantidade em Estoque: <input type="number" name="estoque" min="0" max="10000" required><br>
+    Nome do Produto: <input type="text" name="nome" <?php echo $required; ?>><br>
+        Descrição do Produto: <textarea name="descricao" <?php echo $required; ?>></textarea><br>
+        Preço do Produto: <input type="number" name="preco" step="<?php echo $preco_step; ?>" min="<?php echo $preco_min; ?>" max="<?php echo $preco_max; ?>" <?php echo $required; ?>><br>
+        Quantidade em Estoque: <input type="number" name="estoque" min="<?php echo $estoque_min; ?>" max="<?php echo $estoque_max; ?>" <?php echo $required; ?>><br>
         <input type="submit" value="Salvar">
     </form>
 </body>
